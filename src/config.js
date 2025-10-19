d7849147f796598f0538c1ee600b8599c544c832
GnuhNaut <gnuh.naut.vn@gmail.com>
2025-10-19 20:51:32 +0700

    update

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 // src/config.js
 // export const API_BASE_URL = 'http://localhost:8000/api';
-export const API_BASE_URL = 'https://medicallaw.xuan.media/';
+export const API_BASE_URL = 'https://medicallaw.xuan.media/api';
